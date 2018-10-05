121ca9766e0efd542bf30e7e64e288099a20dd1f
mueller <mueller@mbits.info>
2018-10-05 12:31:04 +0200

    Checkpoint commit as of 2018-10-05-12-31-01.

--- src/Page.php
+++ src/Page.php
@@ -1,54 +1,134 @@
 <?php
 
-namespace Qck\Expression;
+namespace Qck\Html;
 
 /**
- *
+ * App class is essentially the class to start. It is the basic error handler. No code besides the require statement and initialization should be called in any app before.
+ * 
  * @author muellerm
  */
-class Var_ implements Interfaces\Var_
+class Page implements Interfaces\Page, Interfaces\PageGuard
 {
 
-  function __construct( $Name, $FilterOut = false )
+  function getPage( $BodyTemplateOrText )
   {
-    $this->Name = $Name;
-    $this->FilterOut = $FilterOut;
+    $this->BodyTemplateOrText = $BodyTemplateOrText;
+    return $this;
   }
 
-  function getName()
+  function setLanguageProvider( \Qck\App\Interfaces\LanguageProvider $LanguageProvider )
   {
-    return $this->Name;
+    $this->LanguageProvider = $LanguageProvider;
   }
 
-  public function toSql( \Qck\Sql\Interfaces\DbDialect $Dictionary,
-                         array &$Params = array () )
+  public function addCssLink( $href, $integrity = null, $crossOrigin = null )
   {
-    return $this->Name;
+    $this->CssLinks = [ $href, $integrity, $crossOrigin ];
   }
 
-  function getValue( array $Data = [], array &$FilteredData = [] )
+  public function addJavaScript( $src, $integrity = null, $crossOrigin = null,
+                                 $PlaceBeforeBodyEndTag = true )
   {
-    $Value = isset( $Data[ $this->Name ] ) ? $Data[ $this->Name ] : null;
-    if ( $this->FilterOut == false )
-      $FilteredData[ $this->Name ] = $Value;
-    return $Value;
+    $this->Scripts = [ $src, $integrity, $crossOrigin, $PlaceBeforeBodyEndTag ];
   }
 
-  function __toString()
+  public function getAdditionalHeaders()
   {
-    return $this->Name;
+    return [];
+  }
+
+  public function getCharset()
+  {
+    return \Qck\App\Interfaces\Output::CHARSET_UTF_8;
+  }
+
+  public function getContentType()
+  {
+    return \Qck\App\Interfaces\Output::CONTENT_TYPE_TEXT_HTML;
+  }
+
+  public function render()
+  {
+    return $this->renderHtml();
+  }
+
+  public function renderHtml()
+  {
+    ob_start();
+    $lang = $this->printAttributeIfSet( "lang", $this->LanguageProvider ? $this->LanguageProvider->get() : null );
+    ?>
+    <!doctype html>
+    <html<?= $lang ?>>
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <?php
+        foreach ( $this->CssLinks as $CssLink ):
+          $integrity = $this->printAttributeIfSet( "integrity", $CssLink[ 1 ] );
+          $crossorigin = $this->printAttributeIfSet( "crossorigin", $CssLink[ 2 ] );
+          ?>
+          <link rel="stylesheet" href="<?= $CssLink[ 0 ] ?>"<?= $integrity . $crossorigin ?>>
+        <?php endforeach; ?>
+
+
+        <title><?= $this->Title ?></title>
+      </head>
+      <body>
+        <?= is_string( $this->BodyTemplateOrText ) ? $this->BodyTemplateOrText : $this->BodyTemplateOrText->renderHtml() ?>
+
+        <?php
+        foreach ( $this->Scripts as $Script ):
+          $integrity = $this->printAttributeIfSet( "integrity", $Script[ 1 ] );
+          $crossorigin = $this->printAttributeIfSet( "crossorigin", $Script[ 2 ] );
+          ?>
+          <script src="<?= $Script[ 0 ] ?>"<?= $integrity . $crossorigin ?>></script>        
+        <?php endforeach; ?>
+
+      </body>
+    </html>
+    <?php
+    return ob_get_clean();
+  }
+
+  protected function printAttributeIfSet( $attName, $attValue, $addPrependingSpace = true )
+  {
+    echo is_null( $attValue ) ? "" : ($addPrependingSpace ? " " : "") . $attName . '="' . $attValue . '"';
+  }
+
+  public function setTitle( $Title )
+  {
+    $this->Title = $Title;
   }
 
   /**
    *
    * @var string
    */
-  protected $Name;
+  protected $Title;
 
   /**
    *
-   * @var bool
+   * @var mixed
    */
-  protected $FilterOut;
+  protected $BodyTemplateOrText;
+
+  /**
+   *
+   * @var \Qck\App\Interfaces\LanguageProvider
+   */
+  protected $LanguageProvider;
+
+  /**
+   *
+   * @var array
+   */
+  protected $CssLinks = [];
+
+  /**
+   *
+   * @var array
+   */
+  protected $Scripts = [];
 
 }
